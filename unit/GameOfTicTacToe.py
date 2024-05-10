4084e5c17ff0f1d60bc022aa74f8d41c639187bd
admin <test@test.ru>
2024-05-10 19:22:38 +0300

    Занятие 10 мая: Игра крестики и нолики (дополнен код)

--- unit/GameOfTicTacToe.py
+++ unit/GameOfTicTacToe.py
@@ -1,4 +1,18 @@
 def winxy(s):
-    if s[0] == s[1] and s[1] == s[2]:
+    if s[0] == s[1] and s[1] == s[2] and s[0] != "0":
+        return True
+    if s[3] == s[4] and s[4] == s[5] and s[3] != "0":
+        return True
+    if s[6] == s[7] and s[7] == s[8] and s[6] != "0":
+        return True
+    if s[0] == s[3] and s[3] == s[6] and s[0] != "0":
+        return True
+    if s[1] == s[4] and s[4] == s[7] and s[1] != "0":
+        return True
+    if s[2] == s[5] and s[5] == s[7] and s[2] != "0":
+        return True
+    if s[0] == s[4] and s[4] == s[8] and s[0] != "0":
+        return True
+    if s[2] == s[4] and s[4] == s[6] and s[2] != "0":
         return True
     return False
--- unit/GameOfTicTacToeMessageWin.py
+++ unit/GameOfTicTacToeMessageWin.py
@@ -1,2 +1,18 @@
 def winmessage(s):
-	return "WIN"
+    if s[0] == s[1] and s[1] == s[2] and s[0] != "0":
+        return "Победил " + s[0]
+    if s[3] == s[4] and s[4] == s[5] and s[3] != "0":
+        return "Победил " + s[3]
+    if s[6] == s[7] and s[7] == s[8] and s[6] != "0":
+        return "Победил " + s[6]
+    if s[0] == s[3] and s[3] == s[6] and s[0] != "0":
+        return "Победил " + s[0]
+    if s[1] == s[4] and s[4] == s[7] and s[1] != "0":
+        return "Победил " + s[1]
+    if s[2] == s[5] and s[5] == s[7] and s[2] != "0":
+        return "Победил " + s[2]
+    if s[0] == s[4] and s[4] == s[8] and s[0] != "0":
+        return "Победил " + s[0]
+    if s[2] == s[4] and s[4] == s[6] and s[2] != "0":
+        return "Победил " + s[2]
+	return "Победитель определен"
